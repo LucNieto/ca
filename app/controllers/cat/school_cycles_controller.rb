b3bfc0772839e18e14560d7f5d0ae9ca032d53aa
Uziel Trujillo <uziel.trujillo@gmail.com>
2017-04-08 01:44:19 -0500

    [#143358655] Guardando Ciclos Escolares
    Nota:
    - Se crea el codigo necesario para guardar la información del ciclo escolar en la base de datos
    	modified:   app/controllers/cat/school_cycles_controller.rb

--- app/controllers/cat/school_cycles_controller.rb
+++ app/controllers/cat/school_cycles_controller.rb
@@ -9,9 +9,28 @@ class Cat::SchoolCyclesController < ApplicationController
     @school_cycle = SchoolCycle.new
   end
 
+  def create
+    @school_cycle = SchoolCycle.new(school_cycle_params)
+
+    respond_to do |format|
+      if @school_cycle.save
+        format.html { redirect_to [ "cat", @school_cycle ], notice: 'El ciclo ha sido creado' }
+        format.json { render :show, status: :created, location: @school_cycle }
+      else
+        format.html { render :new }
+        format.json { render json: @school_cycle.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   def show
   end
 
   def edit
   end
+
+  private
+    def school_cycle_params
+      params.require(:school_cycle).permit(:career_id, :nombre_ciclo)
+    end
 end
