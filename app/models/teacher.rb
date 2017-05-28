db846930ed06577b2503bfcf42610c1c353f2a4c
Uziel Trujillo <uziel.trujillo@gmail.com>
2017-05-28 16:17:55 -0500

    Actualizando asociacion entre materias - maestro
    Notas:
    - Se agrega select2 como gema
    - Se eliminan js.coffee y scss innecesariso
    - Se corrigen algunas cuestiones de logica ligeras
    	modified:   Gemfile
    	modified:   Gemfile.lock
    	new file:   app/assets/javascripts/_select2.js.coffee
    	modified:   app/assets/javascripts/application.js
    	deleted:    app/assets/javascripts/periods.coffee
    	deleted:    app/assets/javascripts/school_cycle_has_subjects.coffee
    	deleted:    app/assets/javascripts/teachers.coffee
    	modified:   app/assets/stylesheets/application.css
    	deleted:    app/assets/stylesheets/periods.css.less
    	deleted:    app/assets/stylesheets/teachers.css.less
    	modified:   app/models/teacher.rb
    	modified:   app/views/asig/school_cycle_has_subjects/_form.html.erb
    	modified:   app/views/asig/school_cycle_has_subjects/index.html.erb
    	modified:   app/views/asig/school_cycle_has_subjects/show.html.erb
    	modified:   app/views/cat/periods/index.html.erb
    	modified:   app/views/cat/teachers/index.html.erb
    	modified:   db/schema.rb

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -3,4 +3,8 @@ class Teacher < ApplicationRecord
   validates :apellido_paterno, :presence => true
   validates :grado, :presence => true
   validates :correo_electronico, :presence => true
+
+  def nombre_completo
+     "#{nombre} #{apellido_paterno} #{apellido_materno}"
+  end
 end
