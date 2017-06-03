1c33033a19072d88de39e2143009eb03021c01c3
Luc <noodle3030@hotmail.com>
2017-06-03 16:21:18 -0500

    agregando link al menu

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,4 +1,6 @@
 class Attendance < ApplicationRecord
   belongs_to :student_has_subject
   enum attendance_type: ['Asistio','Retardo','Falta', 'Justificante']
+
+  validates :attendance_type,  :presence => true
 end
